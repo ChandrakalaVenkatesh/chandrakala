8270d85441cc506e3c625ba941855b1a32753c3a
ChandrakalaVenkatesh <120547355+ChandrakalaVenkatesh@users.noreply.github.com>
2023-03-20 20:33:55 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,27 +1,56 @@
-import React,{Component} from "react"
-import JSON from "./user.json"
-import Users from './Users.jsx';
-class App extends Component{
+ import React,{useRef} from 'react'
+// import {ToastContainer,toast} from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
+// const App=()=>{
+//     let handleSubmit=()=>{
+//         toast.success("Logged in successfully",{position:toast.POSITION.TOP_CENTER})
+//     }
+//         return(
+//             <div>
+//                 <ToastContainer/>
+//                 <button onClick={handleSubmit}> click for change</button>
+//             </div>
+//         )
+// }
+//export default App;
+// export default class App extends Component {
+//     constructor()
+//     {
+//         super()
+//         this.state={
+//             username:""
+//         }
+//     }
+    // componentDidMount()     //non-synthetic event 
+    // {
+    //     let input=document.querySelector('input')
+    //     input.addEventListener('keypress',(e)=>{
+    //         this.setState({username:e.target.value})
+    //     })
+    // }
+  //render() {
+   // return (
+      <>
+      {/* <h1>{this.state.username}</h1>   //synthetic  event
+      <input type="text" onChange={(e)=>{
+        this.setState({username:e.target.value})
+      }} /> */}
+      </>
 
-
-
-constructor()
+const App=()=>
 {
-    super()
-    this.state={
-        json:JSON
+    let h1Ref=useRef()
+    let handleChange=()=>
+    {
+      h1Ref.current.style.color="green"
     }
-}
+      return(
+        <>
+        <h1 ref={h1Ref}>APPPP</h1>
+        <button onClick={handleChange}>change</button>
+        </>
+      )
+    }
 
-render()
-{
-    return(
-        <div>
-            <Users data={this.state.json}/>
-        </div>
-    )
-}
-}
-
-export default App;
+export default App
